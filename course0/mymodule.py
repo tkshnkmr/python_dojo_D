256b33322a820fde983318d652211a0e1a3f62e3
tkshnkmr <tksh.nakamura@gmail.com>
2022-06-07 11:01:39 +0100

    added sample module

---
+++ course0/mymodule.py
@@ -0,0 +1,33 @@
+"""
+This script is ... brah brah ...
+"""
+
+from typing import List
+
+
+def my_method_1(x_val: int, y_val: str) -> str:
+    """
+    My method
+
+    Args:
+        x_val (Integer): input
+        y_val (str): input
+    Returns:
+        str
+    """
+
+    return str(x_val) + y_val
+
+
+def _my_hidden_method_1(x_val: int, y_val: str) -> List:
+    """
+    My hidden method
+
+    Args:
+        x_val (Integer): input
+        y_val (str): input
+    Returns:
+        List
+    """
+
+    return [str(x_val), y_val]
