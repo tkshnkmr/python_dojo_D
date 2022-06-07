b2da36a00cb6f2011bf983bdcb8c39cab8aacda1
tkshnkmr <tksh.nakamura@gmail.com>
2022-06-07 10:56:40 +0100

    updated bad script

--- course0/my_bad_python_code.py
+++ course0/my_bad_python_code.py
@@ -5,8 +5,11 @@ name='name'
 
 def my_method(x, y):
    # No doc string
+   # No typing
    return x + y
 
 
-
+# Long comment, Long comment, Long comment, Long comment, Long comment, Long comment, Long comment, Long comment, 
 y=2
+
+z = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 1000, 5000, 6000, 10, 100, 200, 300]
